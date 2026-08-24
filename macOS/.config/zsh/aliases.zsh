e0950275d31d5215289a618037b82287824aabc1
csmphd <choseokmin@icloud.com>
2026-08-24 23:48:58 +0900

    edit aliases.zsh

--- macOS/.config/zsh/aliases.zsh
+++ macOS/.config/zsh/aliases.zsh
@@ -1,22 +1,28 @@
+# ls aliases: linux bash configuration
+#alias l='ls -AF'
+#alias la='ls -A'
+#alias ll='ls -Alh'
+#alias ls='ls --color'
+
 # eza: a modern replacement for "ls"
-
-# Better ls
-alias ls='eza --icons=auto'
-
-# Detailed listing
-alias ll='eza -lh --icons --git'
-
-# Detailed listing including hidden files
-alias la='eza -lah --icons --git'
-
-# Tree view
-alias tree='eza --icons --tree'
-
-# Reuse ls completions for eza
-# (avoids defining a separate completion function)
-compdef eza=ls
+if command -v eza >/dev/null 2>&1; then
+    # Set EZA environment varibles
+    export EZA_ICONS_AUTO=1;
+    export EZA_ICONS_SPACE=2;
+    # Better ls
+    alias ls='eza -AF=always';
+    # Detailed listing including hidden files
+    alias la='eza -a';
+    # Detailed listing
+    alias ll='eza -alghH --smart-group --git';
+    # Tree view
+    alias tree='eza -alh --no-user --git --tree --level=4';
+    # Reuse ls completions for eza
+    # (avoids defining a separate completion function)
+    compdef eza=ls;
+fi
 
 # bat: a "cat" clone with syntax highlighting and Git integration
-
-# Better cat
-alias cat='bat'
+# Better cat, some distro install `bat` as `batcat`
+[[ "command -v bat" ]] && alias cat='bat'
+[[ "command -v batcat" ]] && alias cat='batcat'
